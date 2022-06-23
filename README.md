9fcfefb3dbed4203bfde63974b57dc7a3d033964
Late-Is-Cool <LateTheIdiot@outlook.com>
2022-06-22 21:36:52 -0500

    initial commit (actually existed here not fake)

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# wasteof.js
-a module that allows you to access the wasteof.money api but not fully i think
+<div align="center">
+    <br/>
+    <a href="https://wasteof.money/users/late"><img src="https://user-images.githubusercontent.com/78447219/174464606-7829acea-80e6-49f8-8be0-dce67e7af8f1.png" alt="Logo"/></a>
+    <br/>
+    <p>
+        <a href="https://www.npmjs.com/package/wasteof.js"><img src="https://img.shields.io/npm/v/wasteof.js.svg?maxAge=3600" alt="npm version" /></a>
+        <a href="https://www.npmjs.com/package/wasteof.js"><img src="https://img.shields.io/npm/dt/wasteof.js.svg?maxAge=3600" alt="npm downloads" /></a>
+        <a href="https://github.com/Late-Is-Cool/wasteof.js/actions"><img src="https://github.com/Late-Is-Cool/wasteof.js/actions/workflows/main.yml/badge.svg" alt="Tests status" /></a>
+    </p>
+</div>
+
+## about
+
+wasteof.js is a <a href="https://nodejs.org/">node.js</a> module that allows you to react with the <a href="https://wasteof.money/">wasteof.money</a> api
+
+- i dont know
+- not complete coverage of the api (i think)
+- socket.io included
+
+## installation
+
+**node.js version needs.. i dont know, just make sure its up to date i guess**
+
+```sh-session
+npm install wasteof.js
+```
+
+## example usage
+
+```js
+const wasteof = require("wasteof.js")
+const client = new wasteof.Client()
+
+client.on("ready", () => {
+    console.log("Logged in!")
+})
+
+client.on("onMention", (data) => {
+    if (data.mention?.type === "wall_comment") {
+        client.postWallComment("late", "heya!", data.mention.data.comment._id).catch((error) => console.log(error))
+    }
+})
+
+client.login("late", "FunnyAmongus123#5812")
+```
---
+++ src/index.js
@@ -0,0 +1,174 @@
+const axios = require("axios");
+const { io } = require("socket.io-client");
+
+class Client {
+    constructor() {
+        this.socket = io("https://api.wasteof.money/", {
+            auth: {
+                token: this.token,
+            },
+        });
+    }
+    login(username, password) {
+        let json;
+        axios({
+            data: {
+                username: username,
+                password: password,
+            },
+            url: "https://api.wasteof.money/session",
+            method: "post",
+        }).then((response) => {
+            json = response.data;
+            this.token = json.token;
+            this.socket.emit("updateUser", this.token);
+        });
+        return json;
+    }
+    async on(event, callback) {
+        const parsedEvent = event.toLowerCase();
+        if (parsedEvent === "ready") {
+            let on = false;
+            this.socket.on("connect", () => {
+                if (!on) {
+                    on = !on;
+                    this.socket.emit("updateUser", this.token);
+                    callback();
+                }
+            });
+        } else if (parsedEvent === "onmention") {
+            var oldCount = 0;
+            let on = false
+            this.socket.on("updateMessageCount", async (count) => {
+                if (!on) {
+                    oldCount = count 
+                    on = true
+                } else {
+                    let mention = await this.getUnreadMessages()
+                    if (oldCount < count) {
+                        oldCount = count;
+                        callback({
+                            count: count,
+                            mention: mention.unread[0]
+                        });
+                    }
+                }
+                
+            });
+        } else if (parsedEvent === "chatmessage") {
+            this.socket.on("message", (data) => {
+                callback({
+                    data: data
+                })
+            })
+        } else {
+            console.error("wasteof.js >> invalid event name, current event names: " + "chatMessage, onMention, ready")
+            return
+        }
+    }
+    async getUnreadMessages(page) {
+        const response = await axios({
+            headers: {
+                "Authorization": this.token,
+            },
+            url: `https://api.wasteof.money/messages/unread?page=${page}`,
+            method: "get",
+        });
+        const json = await response.data;
+        return json;
+    }
+    async getSession() {
+        const response = await axios({
+            headers: {
+                "Authorization": this.token
+            },
+            url: "https://api.wasteof.money/session",
+            method: "get"
+        })
+        const json = await response.data
+        return json
+    }
+    async post(content, repost) {
+        const response = await axios({
+            headers: {
+                "Authorization": this.token
+            },
+            data: {
+                post: content,
+                repost: repost
+            },
+            url: "https://api.wasteof.money/posts",
+            method: "post"
+        })
+        const json = await response.data
+        return json
+    }
+    async editPost(id, content) {
+        const response = await axios({
+            headers: {
+                "Authorization": this.token
+            },
+            data: {
+                post: content
+            },
+            url: `https://api.wasteof.money/posts/${id}`,
+            method: "put"
+        })
+        const json = await response.data
+        return json
+    }
+    async deletePost(id) {
+        const response = await axios({
+            headers: {
+                "Authorization": this.token
+            },
+            url: `https://api.wasteof.money/posts/${id}`,
+            method: "delete"
+        })
+        const json = await response.data
+        return json
+    }
+    async postComment(postid, content, parent) {
+        const response = await axios({
+            headers: {
+                "Authorization": this.token
+            },
+            url: `https://api.wasteof.money/posts/${postid}/comments`,
+            method: "post",
+            data: {
+                content: content,
+                parent: parent
+            }
+        })
+        const json = await response.data
+        return json
+    }
+    async deleteComment(id) {
+        const response = await axios({
+            headers: {
+                "Authorization": this.token
+            },
+            url: `https://api.wasteof.money/comments/${id}`,
+            method: "delete"
+        })
+        const json = await response.data
+        return json
+    }
+    async postWallComment(user, content, parent) {
+        const response = await axios({
+            headers: {
+                "Authorization": this.token
+            },
+            url: `https://api.wasteof.money/users/${user}/wall`,
+            method: "post",
+            data: {
+                content: content,
+                parent: parent
+            }
+        })
+        const json = await response.data
+        return json
+    }
+}
+
+module.exports.Client = Client;
